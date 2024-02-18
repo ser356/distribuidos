ec205eee743cc4763578f91a105726d050b0f81d
ser356 <70541693+ser356@users.noreply.github.com>
2024-02-18 14:42:45 +0100

    RELEVOS WITH MAVEN ARCH

---
+++ src/test/java/com/practica/carrera/CarreraTest.java
@@ -0,0 +1,18 @@
+package com.practica.carrera;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class CarreraTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
